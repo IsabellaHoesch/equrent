f1921e3bd5cd4fdbf6cc95a6354d67cb3e2db79b
Tea Filipovic <tea.filipovic0@gmail.com>
2021-05-25 16:42:52 +0200

    New and create are added (controller and view)

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -4,4 +4,25 @@ class OffersController < ApplicationController
   def index
     @offers = Offer.all
   end
+
+  def new
+    @offer = Offer.new
+  end
+
+  def create
+    @user = current_user
+    @offer = Offer.new(offer_params)
+    @offer.user = @user
+    if @offer.save
+      redirect_to offers_path, notice: "Offer was successfully created."
+    else
+      render :new
+    end
+  end  
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:offer_type, :description, :offer_img, :sport_type, :price, :address)
+  end
 end
