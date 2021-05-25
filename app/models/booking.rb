3f6f869daf36d4b5997755a5dc6083115d0aae26
Armen Kaltak <arkouo@gmail.com>
2021-05-25 14:46:21 +0200

    added validations to booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :offer
+  validates :start_date, :end_date, presence: true
+  # validates :start_date, not_in_past: true
+  validates :end_date, date: { after_or_equal_to: :start_date }
 end
