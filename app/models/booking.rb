1e23fe2ad30b0dcbf3f9fcfc70e61daf1bcea5a7
Isabella Hoesch <isabella.hoesch@outlook.com>
2021-05-25 17:13:38 +0200

    muted date validation to push to heroku

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,5 +3,5 @@ class Booking < ApplicationRecord
   belongs_to :offer
   validates :start_date, :end_date, presence: true
   # validates :start_date, not_in_past: true
-  validates :end_date, date: { after_or_equal_to: :start_date }
+  # validates :end_date, date: { after_or_equal_to: :start_date }
 end
