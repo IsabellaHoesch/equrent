94cc4d016de3f0199a11c60371a023ec7dc58d33
Isabella Hoesch <isabella.hoesch@outlook.com>
2021-05-26 15:12:16 +0200

    style changes

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -3,7 +3,7 @@ class Offer < ApplicationRecord
   has_many :bookings
   SPORT_TYPES = %w(basketball football volleyball ping-pong ski hiking kanu rackets treeclimbing)
   validates :offer_type, :name, :description, :address, :price, :offer_img, :sport_type, presence: true
-  validates :description, length: { minimum: 20, message: "Description has to have at least 20 characters" }
+  validates :description, length: { minimum: 10, message: "Description has to have at least 20 characters" }
   validates :offer_type, inclusion: { in: %w(equipment facility), message: "Please choose between 'equipment' or 'facility'." }
   validates :sport_type, inclusion: { in: SPORT_TYPES, message: "Please choose one from the list." }
   validates :price, length: { in: 1..100 }
