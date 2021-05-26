3c87b129ade44f01013fe110002266c7b0e81789
Armen Kaltak <arkouo@gmail.com>
2021-05-26 14:50:41 +0200

    Just seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,3 +30,7 @@ puts "#{user.name}"
 end
 
 puts "Successfully seeeeeeeeeeeedeeeeeeeeeeeed xD"
+
+Offer.create(name: "Hiking shoes", offer_type:"equipment", description: "Hiking shoes from company colombia bought 1 year ago", sport_type: "hiking", address: "Varnhagenstrasse 24, München", offer_img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.qishKHwBYsPxz8VzHbJ0SwHaEK%26pid%3DApi&f=1", price: 20, user: user1)
+Offer.create(name: "A ball to shoot", offer_type: "equipment", description: "a ball from euro2008", sport_type: "football", address: "Perlacher Strasse 20, München", offer_img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ebayimg.com%2Fimages%2Fg%2FWDEAAOSwIKVeM-Tn%2Fs-l300.jpg&f=1&nofb=1", price: 15, user: user2)
+Offer.create(name: "Kanu boot", offer_type: "equipment", description: "Kanu kajak from v. Wildholz", sport_type: "kanu", address: "Jahnstrasse 10, Ottobrunn", offer_img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2F72%2F74%2F53%2F7274535bad06c571e5f652ea19fda109--wood-wood-wood-art.jpg&f=1&nofb=1", price: 70, user: user1)
