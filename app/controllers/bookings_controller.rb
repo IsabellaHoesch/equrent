328bee8b621f7d02628ca78fa500dbd2dca3343a
Armen Kaltak <arkouo@gmail.com>
2021-05-27 16:06:24 +0200

    Defined the create method tested it and it works. Added when something is booked its gone

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,4 +3,22 @@ class BookingsController < ApplicationController
     @offer = Offer.find(params[:offer_id])
     @booking = Booking.new
   end
+
+  def create
+    @offer = Offer.find(params[:offer_id])
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.offer = @offer
+    if @booking.save
+      redirect_to offers_path, notice: "Booked successfully."
+    else
+      render "offers/show"
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :price)
+  end
 end
