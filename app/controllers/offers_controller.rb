73338ee6084e0b33800004a0a4e0e4b7d65cc08e
Isabella Hoesch <isabella.hoesch@outlook.com>
2021-05-27 11:38:15 +0200

    added markers and popup to map

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -9,7 +9,7 @@ class OffersController < ApplicationController
         lat: offer.latitude,
         lng: offer.longitude,
         info_window: render_to_string(partial: "info_window", locals: { offer: offer }),
-        image_url: helpers.asset_url('icon.png')
+        image_url: helpers.asset_url('200w.webp')
       }
     end
   end
